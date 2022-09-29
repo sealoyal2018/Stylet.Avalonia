08cc6a884002995358ea6867bf45a23e176e8753
sealoyal <sealoyal@hotmail.com>
2022-09-29 14:06:41 +0800

    initialized

---
+++ Avalonia.Stylet/Avalonia.Stylet/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Stylet
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
