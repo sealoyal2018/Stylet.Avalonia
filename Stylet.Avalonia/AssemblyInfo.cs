55ad460dab5de6896cf97a09517b03497a422e3e
sealoyal <sealoyal@hotmail.com>
2023-12-18 14:16:57 +0800

    feat: 支持 axaml 前缀引入

---
+++ Stylet.Avalonia/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Avalonia.Metadata;
+
+[assembly: XmlnsDefinition("https://github.com/sealoyal2018/stylet.avalonia", "Stylet.Xaml")]
+[assembly: XmlnsPrefix("https://github.com/sealoyal2018/stylet.avalonia", "s")]
